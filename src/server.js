98a841f2ad1d9e9d3c4b9a67ba058d1f0263a02f
loegnah <loegnah@gmail.com>
2021-12-11 19:04:50 +0900

    Router 2

--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import morgan from "morgan";
-
-import globalRouter from "./Router/globalRouter";
-import userRouter from "./Router/userRouter";
-import videoRouter from "./Router/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
 const app = express();
 const logger = morgan("dev");
-
 app.use(logger);
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
 
-const handleListen = () => {
-  console.log(`Server is Connected : http://localhost:${PORT}`);
-};
-app.listen(PORT, handleListen);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
