fa36736262428a815c5fc1e7ba1b456d3adccfc1
loegnah <loegnah@gmail.com>
2022-01-28 00:57:23 +0900

    Apply dotenv

---
+++ README.md
@@ -0,0 +1,6 @@
+If you use this project, you can make .env file and set these values.
+
+- COOKIE_SECRET
+  - used by session middleware as secret option
+- DB_URL
+  - mongo DB URL
--- src/server.js
+++ src/server.js
@@ -19,10 +19,13 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "hello",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    cookie: {
+      maxAge: 20000,
+    },
   })
 );
 app.use(localsMiddleware);
