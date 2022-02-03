a3e0c4abb27e360b09b643f7463b832a10f99f95
Loegnah <loegnah@github.com>
2022-02-04 00:00:15 +0900

    protector pulic middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,20 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  if (res.locals.loggedIn) {
-    res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
+  next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return res.redirect("/");
   }
   next();
 };
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -6,12 +6,17 @@ import {
   postLogin,
 } from "../controllers/userController";
 import { home, searchVideo } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.route("/search").get(searchVideo);
 
 export default rootRouter;
